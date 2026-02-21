4507a57539bac2eebf26a8e8414f5b1013c8f9c4
DIVYA-KAREGAONKAR <kbtug23390@kbtcoe.org>
2026-02-21 20:29:41 +0530

    Added pymysql dependency and updated init

--- domestyx-backend/domestyx_backend/__init__.py
+++ domestyx-backend/domestyx_backend/__init__.py
@@ -3,6 +3,7 @@ pymysql.install_as_MySQLdb()
 import pymysql'''
 import pymysql
 
+
 # This must come BEFORE install_as_MySQLdb()
 pymysql.version_info = (2, 2, 1, "final", 0)
 pymysql.install_as_MySQLdb()
