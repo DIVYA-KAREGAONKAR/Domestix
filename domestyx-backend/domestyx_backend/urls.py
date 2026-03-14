ec718ae08052264a5ecf054075f3a0f5e1517cee
DIVYA-KAREGAONKAR <kbtug23390@kbtcoe.org>
2026-03-14 14:08:45 +0530

    responsive design

--- domestyx-backend/domestyx_backend/urls.py
+++ domestyx-backend/domestyx_backend/urls.py
@@ -1,10 +1,14 @@
 # domestyx_backend/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 # domestyx_backend/urls.py
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),  # 👈 Empty string means no prefix
     path('', include('jobs.urls')),
+    # React SPA fallback (serves index.html for client-side routes)
+    re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
